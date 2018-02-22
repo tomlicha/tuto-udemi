3dac9848c1fce432e9cd612e90af35c54e033cd7
Unknown <tomylicha@hotmail.fr>
2018-02-22 10:35:57 +0100

    implementing respawn on bullet hit
    i

---
+++ Assets/Script/BulletScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("player dead");
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider Other) {
+		if (Other.gameObject.tag == "Player"){
+			Other.gameObject.transform.position = GameObject.Find("SpawnPoint").transform.position;
+		}
+	}
+}
