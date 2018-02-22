21e50b9260b380ef49f081b20fe8ca9d4a415da4
Unknown <tomylicha@hotmail.fr>
2018-02-22 14:30:49 +0100

    first menu finished, exit button to leave the game at the begining and a play button to launch the lvl 1

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void Exit(){
+		Debug.Log("exit");
+		Application.Quit();
+	}
+	public void Play(){
+		SceneManager.LoadScene("scene1");
+	}
+}
