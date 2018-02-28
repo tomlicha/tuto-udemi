11437fd9e16e78d10b8e689a304f425445237a7d
Unknown <tomylicha@hotmail.fr>
2018-02-28 09:58:16 +0100

    adding a boat, moving between 2 plateforms, and able to carry the player

---
+++ Assets/Script/boat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boat : MonoBehaviour {
+
+	// Use this for initialization
+void OnTriggerEnter (Collider Other) {
+		if (Other.gameObject.tag == "Player"){
+			Other.transform.SetParent(this.transform);
+		}
+	}
+	
+	// Update is called once per frame
+void OnTriggerExit (Collider Other) {
+		if (Other.gameObject.tag == "Player"){
+			Other.transform.parent=null;
+		}
+	}
+}
